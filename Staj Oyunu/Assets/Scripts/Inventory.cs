fcdb79d72afb7889e8da242ce36c620dc7f3e908
Oktay Halıgür <59488967+sssbug@users.noreply.github.com>
2024-07-29 10:27:22 +0300

    29.07.2024

--- Staj Oyunu/Assets/Scripts/Inventory.cs
+++ Staj Oyunu/Assets/Scripts/Inventory.cs
@@ -9,8 +9,30 @@ namespace DS
 {
     public class Inventory : MonoBehaviour
     {
-       
+        private Sprite _objectImage;
+        private GameObject _myObject;
+        private string _name;
+        public Inventory(Sprite objectImage, string name)
+        {
+            _objectImage = objectImage;
+            _name = name;
 
+            Spawner();
+            
+            
+        }
+        void Spawner()
+        {
 
+            GameObject theObject = new GameObject(_name);
+            theObject.AddComponent<CanvasRenderer>();
+            theObject.AddComponent<Objects>();
+            theObject.AddComponent<Image>().sprite = _objectImage;
+
+            _myObject = Instantiate(theObject);
+
+            _myObject.gameObject.transform.parent = GameObject.Find("SlotBox").gameObject.transform;
+            
+        }
     }
 }
--- Staj Oyunu/Assets/Scripts/Objects.cs
+++ Staj Oyunu/Assets/Scripts/Objects.cs
@@ -1,21 +1,43 @@
-﻿using System.Collections;
+﻿//using DialogueEditor;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace DS
 {
     public class Objects : MonoBehaviour, IPointerClickHandler
     {
+        //private NPCConversation _myConvarsation;
+
+
+
+        private void Start()
+        {
+            //if (TryGetComponent<NPCConversation>(out NPCConversation nPC))
+            //{
+            //    _myConvarsation = GetComponent<NPCConversation>();
+            //}
+        }
+
         public void OnPointerClick(PointerEventData eventData)
         {
-            // Tıklanan nesne
+
+
+            
             GameObject clickedObject = eventData.pointerPress;
 
             if (clickedObject != null && clickedObject.tag == "1")
             {
-                Debug.Log("Oldu");
+               //ConversationManager.Instance.StartConversation(_myConvarsation);
+               Inventory halı = new Inventory(clickedObject.GetComponent<Image>().sprite, "hali");
             }
+            if (clickedObject != null && clickedObject.name == "hali(Clone)")
+            {
+                Debug.Log(222);
+            }
+            
         }
     }
 }
